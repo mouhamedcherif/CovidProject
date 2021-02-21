7a4e9360219a75bd93591e0b6db726e49cd4ebba
medcherif <mohamedcherif.haddad@esprit.tn>
2021-02-21 20:41:19 +0100

    first commit

---
+++ src/main/java/com/esprit/covid/Covid1Application.java
@@ -0,0 +1,13 @@
+package com.esprit.covid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Covid1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Covid1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/esprit/covid/Covid1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esprit.covid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Covid1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
